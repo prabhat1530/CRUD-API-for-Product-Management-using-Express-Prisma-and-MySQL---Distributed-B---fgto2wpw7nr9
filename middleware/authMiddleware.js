479ad76a04f4decbf2143ceea922fc872721b35d
prabhat1530 <152500533+prabhat1530@users.noreply.github.com>
2024-12-02 09:42:31 +0530

    ok

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,15 @@
 const validKey = "8a60348b-d4a4-564a-9b45-aab518adb7f4";
+const dotenv = require('dotenv')
+dotenv.config();
 
 const authMiddleware = (req, res, next) => {
   const apiauthkey = req.headers("apiauthkey")
-  console.log(apiauthkey)
 
   if (!apiauthkey) {
     return res.status(403).json({ message: "Access denied, apiauthkey is missing" });
   }
 
-  if (apiauthkey !== validKey) {
+  if (apiauthkey !== process.env.apiauthkey) {
     return res.status(403).json({ message: "Failed to authenticate apiauthkey" });
   }
 
