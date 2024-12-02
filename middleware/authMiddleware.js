71e8f67d1a58b215349d23b9afd29e9bf2f0b298
prabhat1530 <152500533+prabhat1530@users.noreply.github.com>
2024-12-02 09:38:34 +0530

    header changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,8 @@
 const validKey = "8a60348b-d4a4-564a-9b45-aab518adb7f4";
 
 const authMiddleware = (req, res, next) => {
-  const apiauthkey = req.headers.apiauthkey;
+  const apiauthkey = req.headers("apiauthkey")
+  console.log(apiauthkey)
 
   if (!apiauthkey) {
     return res.status(403).json({ message: "Access denied, apiauthkey is missing" });
